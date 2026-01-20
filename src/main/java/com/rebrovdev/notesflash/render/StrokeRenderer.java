36ab20b2074d66945dbfaf578cfc088beea6c631
Rebrov Yura <yurirebrov01@gmail.com>
2026-01-20 19:03:50 +0300

    add: smooth to penTool

--- src/main/java/com/rebrovdev/notesflash/render/StrokeRenderer.java
+++ src/main/java/com/rebrovdev/notesflash/render/StrokeRenderer.java
@@ -6,28 +6,49 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class StrokeRenderer {
 
     public void render(GraphicsContext gc, Stroke stroke) {
 
-        var points = stroke.getPoints();
+        List<Point> points = stroke.getPoints();
 
-        if (stroke.getPoints().size() < 2)
-            return;
+        if (stroke == null) return;
+        if (stroke.getPoints().size() < 2 && points == null) return;
+
+//        var points = stroke.getPoints();
 
         gc.setStroke(Color.BLACK);
-
         gc.setLineWidth(stroke.getWidth());
-
+        gc.setLineCap(javafx.scene.shape.StrokeLineCap.ROUND);
+        gc.setLineJoin(javafx.scene.shape.StrokeLineJoin.ROUND);
         gc.beginPath();
-        gc.moveTo(points.getFirst().getX(), points.getFirst().getY());
+//        gc.moveTo(points.getFirst().getX(), points.getFirst().getY());
 
-        for (int i = 0; i < points.size(); i++) {
-            var p = points.get(i);
-            gc.lineTo(p.getX(), p.getY());
+//        for (int i = 0; i < points.size(); i++) {
+//            var p = points.get(i);
+//            gc.lineTo(p.getX(), p.getY());
+//        }
+
+        Point p0 = points.get(0);
+        gc.moveTo(p0.getX(), p0.getY());
+        for (int i = 0; i < points.size() - 1; i++) {
+            Point p1 = points.get(i);
+            Point p2 = points.get(i + 1);
+
+            double cx = (p1.getX() + p2.getX()) / 2;
+            double cy = (p2.getY() + p2.getY()) / 2;
+
+            double sx = p1.getX() + (cx - p1.getX()) * 0.8;
+            double sy = p1.getY() + (cy - p1.getY()) * 0.8;
+
+            gc.quadraticCurveTo(sx, sy, cx, cy);
         }
+
+        Point last = points.get(points.size() - 1);
+        gc.lineTo(last.getX(), last.getY());
 
         gc.stroke();
     }
--- src/main/java/com/rebrovdev/notesflash/tools/PenTool.java
+++ src/main/java/com/rebrovdev/notesflash/tools/PenTool.java
@@ -1,5 +1,6 @@
 package com.rebrovdev.notesflash.tools;
 
+import com.rebrovdev.notesflash.model.Point;
 import com.rebrovdev.notesflash.model.Stroke;
 
 import java.util.function.Consumer;
@@ -8,8 +9,15 @@ public class PenTool implements Tool {
 
     private final Consumer<Stroke> onStrokeFinished;
     private Stroke currentStroke;
+    private Point lastPoint;
     private int color;
     private double width;
+
+    private double lastX;
+    private double lastY;
+    private boolean firstPoint = true;
+
+    private static final double ALPHA = 0.4;
 
     public PenTool(int color, double width, Consumer<Stroke> onStrokeFinished) {
         this.color = color;
@@ -20,24 +28,39 @@ public class PenTool implements Tool {
 
     @Override
     public void onPress(double x, double y) {
+//        currentStroke = null;
         currentStroke = new Stroke(color, width);
-        currentStroke.addPoint(x, y);
+        firstPoint = true;
+//        currentStroke.addPoint(x, y);
+        addSmoothedPoint(x, y);
     }
 
     @Override
     public void onDrag(double x, double y) {
-        if (currentStroke != null) {
-            currentStroke.addPoint(x, y);
-        }
+//        if (currentStroke != null) {
+//            currentStroke.addPoint(x, y);
+//        }
+        addSmoothedPoint(x, y);
+//        if (lastPoint == null && currentStroke != null) {
+//            lastPoint = new Point(x, y);
+//            currentStroke.addPoint(x, y);
+//            return;
+//        }
+//        double smoothX = (lastPoint.getX() + x) / 2;
+//        double smoothY = (lastPoint.getY() + y) / 2;
+//        currentStroke.addPoint(smoothX, smoothY);
+//        lastPoint = new Point(smoothX, smoothY);
     }
 
     @Override
     public void onRelease(double x, double y) {
         if (currentStroke != null) {
             currentStroke.addPoint(x, y);
+            addSmoothedPoint(x,y);
             onStrokeFinished.accept(currentStroke);
 //            currentStroke = null;
         }
+//        currentStroke = null;
     }
 
     public boolean hasPreview() {
@@ -52,5 +75,22 @@ public class PenTool implements Tool {
         Stroke finish = currentStroke;
         currentStroke = null;
         return finish;
+    }
+
+    private void addSmoothedPoint(double x, double y) {
+
+        if (firstPoint) {
+            lastX = x;
+            lastY = y;
+            firstPoint = false;
+        }
+
+        double smoothX = lastX + (x - lastX) * ALPHA;
+        double smoothY = lastY + (y - lastY) * ALPHA;
+        Point currentPoint = new Point(smoothX, smoothY);
+        currentStroke.addPoint(currentPoint.getX(), currentPoint.getY());
+
+        lastX = smoothX;
+        lastY = smoothY;
     }
 }
